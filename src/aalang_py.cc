c41f324165634e13f923ef9795d48d66941d366f
Pablo Virolainen <pablo.virolainen@intel.com>
2011-11-24 15:30:38 +0200

    dummy implementation for python..

---
+++ src/aalang_py.cc
@@ -0,0 +1,60 @@
+/*
+ * fMBT, free Model Based Testing tool
+ * Copyright (c) 2011, Intel Corporation.
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms and conditions of the GNU Lesser General Public License,
+ * version 2.1, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin St - Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ *
+ */
+
+#include "aalang_py.hh"
+#include "helper.hh"
+
+void aalang_py::set_name(std::string* name)
+{
+}
+
+void aalang_py::set_namestr(std::string* _name)
+{ 
+}
+
+void aalang_py::set_variables(std::string* var)
+{
+}
+
+void aalang_py::set_istate(std::string* ist)
+{
+}
+
+void aalang_py::set_guard(std::string* gua)
+{
+}
+
+void aalang_py::set_body(std::string* bod)
+{
+}
+
+void aalang_py::set_adapter(std::string* ada)
+{
+}
+
+void aalang_py::next_action()
+{
+}
+
+std::string aalang_py::stringify()
+{
+  return std::string("");
+}
+
---
+++ src/aalang_py.hh
@@ -0,0 +1,41 @@
+/*
+ * fMBT, free Model Based Testing tool
+ * Copyright (c) 2011, Intel Corporation.
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms and conditions of the GNU Lesser General Public License,
+ * version 2.1, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin St - Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ */
+#ifndef __aalang_py_hh__
+#define __aalang_py_hh__
+
+
+#include "aalang.hh"
+#include <string>
+#include <vector>
+
+class aalang_py: public aalang {
+public:
+  virtual void set_name(std::string* name);
+  virtual void set_namestr(std::string* name);
+  virtual void set_variables(std::string* var);
+  virtual void set_istate(std::string* ist);
+  virtual void set_guard(std::string* gua);
+  virtual void set_body(std::string* bod);
+  virtual void set_adapter(std::string* ada);
+  virtual void next_action();
+  virtual std::string stringify();
+protected:
+};
+
+#endif
