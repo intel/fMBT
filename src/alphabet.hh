ad54af82ee72271ebec6be7f5208ed2b76f3690a
Pablo Virolainen <pablo.virolainen@intel.com>
2011-12-13 15:39:07 +0200

    Add new class Alphabet

---
+++ src/alphabet.hh
@@ -0,0 +1,37 @@
+/*
+ * fMBT, free Model Based Testing tool
+ * Copyright (c) 2011, Intel Corporation.
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms and conditions of the GNU Lesser General Public License,
+ * version 2.1, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin St - Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ */
+#ifndef __alphabet_hh__
+#define __alphabet_hh__
+
+#include <string>
+
+class Alphabet {
+public:
+  //! Returns names of all actions available.
+  virtual std::vector<std::string>& getActionNames() = 0;
+
+  //! Returns names of all available state propositions
+  virtual std::vector<std::string>& getSPNames()     = 0;
+
+  //! Returns the name of the given action
+  virtual std::string& getActionName(int action)     = 0;
+  
+};
+
+#endif
