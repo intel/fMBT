d4e3bb1f142988e1f156550dacfb1aab72284e12
Antti Kervinen <antti.kervinen@intel.com>
2012-01-30 11:43:48 +0200

    tutorial test cleanup (fmbt output written to log)

--- test/tutorial/run.sh
+++ test/tutorial/run.sh
@@ -59,7 +59,7 @@ testpassed
 teststep "run:$(cat fmbt.1)..."
 rm -rf /tmp/fmbt.mkrmdir
 rm -f mkrmdir.log
-source fmbt.1
+source fmbt.1 >> $LOGFILE
 if [ "$(fmbt-log -f '$tr' mkrmdir.log)" != "coverage reached" ]; then
     echo "coverage was not reached" >> $LOGFILE
     testfailed
