87a35c965e09abcdd792d193185eb5a553e8bb3f
Pablo Virolainen <pablo.virolainen@intel.com>
2012-02-15 12:40:26 +0200

    model_yes

---
+++ src/model_yes.cc
@@ -0,0 +1,52 @@
+/*
+ * fMBT, free Model Based Testing tool
+ * Copyright (c) 2011, Intel Corporation.
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms and conditions of the GNU Lesser General Public License,
+ * version 2.1, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin St - Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ */
+
+#include "model_yes.hh"
+#include "helper.hh"
+
+void Model_yes::set_model(Model* m) 
+{
+  model=m;
+  std::vector<std::string>& an=model->getActionNames();
+  std::vector<std::string>& pn=model->getSPNames();
+
+  action_names=an;
+  prop_names=pn;
+
+  precalc_input_output();
+
+  for(unsigned i=1;i<action_names.size();i++) {
+    if (!is_output(i)) {
+      iact.push_back(i);
+    }
+    act.push_back(i);
+  }
+
+}
+
+void Model_yes::set_props(std::string p)
+{
+  std::vector<std::string> v;
+  props.clear();
+  static std::string sep(" ");
+  strvec(v,p,sep);
+  for(unsigned i=0;i<v.size();i++) {
+    props.push_back(find(prop_names,v[i]));
+  }
+}
---
+++ src/model_yes.hh
@@ -0,0 +1,71 @@
+/*
+ * fMBT, free Model Based Testing tool
+ * Copyright (c) 2011, Intel Corporation.
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms and conditions of the GNU Lesser General Public License,
+ * version 2.1, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin St - Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ */
+#include "model.hh"
+#include <glib.h>
+
+class Model_yes: public Model {
+public:
+  Model_yes(Log& l, std::string& params) :
+    Model(l, params), model(NULL) {
+  }
+
+  virtual ~Model_yes() {
+    if (model) {
+      delete model;
+    }
+  }
+
+  virtual int getActions(int** _act) {
+    *_act=&act[0];
+    return act.size();
+  }
+
+  virtual int getIActions(int** _act) {
+    *_act=&iact[0];
+    return iact.size();
+  }
+
+  virtual int execute(int action) {
+    return action;
+  }
+
+  virtual int getprops(int** pro) {
+    *pro=&props[0];
+    return props.size();
+  }
+
+  virtual bool reset() {
+    return true;
+  }
+
+  virtual void push() {};
+  virtual void pop() {};
+
+  virtual bool init() { return true; };
+
+  void set_model(Model* m);
+  void set_props(std::string p);
+
+protected:
+  std::vector<int> act;
+  std::vector<int> iact;
+  std::vector<int> props;
+
+  Model* model;
+};
