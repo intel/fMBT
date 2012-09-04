1bf5f89453e69a14704ccd60b6a3d1d522acaaaa
Antti Kervinen <antti.kervinen@intel.com>
2012-09-04 12:36:38 +0300

    Fine tuning escaping in new configuration syntax
    Making this work:
    model="lts_remote(bash -c \"...\")"

--- src/helper.cc
+++ src/helper.cc
@@ -166,7 +166,7 @@ void remove_force(std::string& s)
   for(unsigned i=0;i<s.size();i++) {
     switch (s[i]) {
     case '\\': {
-      i++;
+      break;
     }
     default: {
       ss=ss+s[i];
@@ -710,6 +710,7 @@ void param_cut(std::string val,std::string& name,
 	name = val.substr(0,pos);
 	remove_force(name);
 	option = val.substr(pos+1,lstpos-pos-1);
+	remove_force(option);
       } else {
 	// ERROR
       }
@@ -740,6 +741,7 @@ void commalist(std::string& s,std::vector<std::string>& vec) {
       if (depth==0) {
 	// COMMA!
 	pushme=s.substr(lastend,pos-lastend);
+	remove_force(pushme);
 	vec.push_back(pushme);
 	lastend=pos+1;
       }
