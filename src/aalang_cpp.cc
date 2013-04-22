318a778df9bad776904b69804c3c1b946e50e9ff
Pablo Virolainen <pablo.virolainen@intel.com>
2013-04-22 11:12:28 +0300

    C++/AAL

--- src/aalang_cpp.cc
+++ src/aalang_cpp.cc
@@ -44,7 +44,7 @@ aalang_cpp::aalang_cpp(): aalang(),action_cnt(1), tag_cnt(1), name_cnt(0),
 			  istate(NULL),ainit(NULL), aexit(NULL), name(NULL),
 			  tag(false)
 {
-  default_guard="return true;"; 
+  default_guard="return true;//default"; 
   default_body="";
   default_adapter="";
   std::vector<std::string> t;
@@ -86,21 +86,25 @@ void aalang_cpp::set_name(std::string* name,bool first,ANAMETYPE t)
   if (first) {
     tstack.push_back(tag);
     tag=false;
+    name_cnt_stack.push_back(action_cnt);
     name_cnt_stack.push_back(name_cnt);
     name_cnt=0;
+    action_cnt=anames.size();
   }
 
   if (name_cnt==0) {
-    guard_call_construct.push_back("action"+to_string(action_cnt)+"_guard()");
+    guard_call_construct.push_back("action"+to_string(action_cnt)+"_guard(action_names["+to_string(action_cnt) +"])");
     action_guard_call.push_back(to_call(guard_call_construct));
+  } else {
+    action_guard_call.push_back("filler");
   }
 
-  s+="\n//action"+to_string(action_cnt)+": \""+*name+"\"\n";
   anames.push_back(*name);
   aname.back().push_back(*name);
-  delete name;
   amap.push_back(action_cnt);
+  s+="\n//action"+to_string((int)anames.size()-1)+": \""+*name+"\"\n";
   name_cnt++;
+  delete name;
 }
 
 void aalang_cpp::set_namestr(std::string* _name)
@@ -156,13 +160,15 @@ void aalang_cpp::set_pop(std::string* p,const char* file,int line,int col)
 void aalang_cpp::set_tagname(std::string* name,bool first)
 {
   if (first) {
+    name_cnt_stack.push_back(tag_cnt);
     name_cnt_stack.push_back(name_cnt);
     name_cnt=0;
     tstack.push_back(tag);
+    tag_cnt=tname.size();
   }
 
   if (name_cnt==0) {
-    guard_call_construct.push_back("tag"+to_string(tag_cnt)+"_guard()");
+    guard_call_construct.push_back("tag"+to_string(tag_cnt)+"_guard(tag_names["+to_string(tag_cnt)+"])");
     tag_guard_call.push_back(to_call(guard_call_construct));
   }
   s+="\n//tag"+to_string(tag_cnt)+": \""+*name+"\"\n";
@@ -184,16 +190,21 @@ void aalang_cpp::next_tag()
   guard_call_construct.pop_back();
   name_cnt=name_cnt_stack.back();
   name_cnt_stack.pop_back();
+  tag_cnt=name_cnt_stack.back();
+  name_cnt_stack.pop_back();  
 }
 
 void aalang_cpp::set_guard(std::string* gua,const char* file,int line,int col)
 {
+  if (gua!=&default_guard) {
+    *gua="{\n" + *gua + "\n}\n" + default_guard;
+  }
   if (tag) {
-    s+=to_line(file,line)+"bool tag"+to_string(tag_cnt)+"_guard() {\n"+
-      *gua+"}\n";
+    s+=to_line(file,line)+"bool tag"+to_string(tag_cnt)+"_guard(const std::string& name) {\n"+
+      *gua+"\n}\n";
   } else {
-    s+=to_line(file,line)+"bool action"+to_string(action_cnt)+"_guard() {\n"+
-      *gua+"}\n";
+    s+=to_line(file,line)+"bool action"+to_string(action_cnt)+"_guard(const std::string& name) {\n"+
+      *gua+"\n}\n";
   }
   if (gua!=&default_guard) 
     delete gua;
@@ -201,22 +212,22 @@ void aalang_cpp::set_guard(std::string* gua,const char* file,int line,int col)
 
 void aalang_cpp::set_body(std::string* bod,const char* file,int line,int col)
 {
-  s+=to_line(file,line)+"void action"+to_string(action_cnt)+"_body() {\n"+*bod+"}\n";
+  s+=to_line(file,line)+"void action"+to_string(action_cnt)+"_body(const std::string& name) {\n"+*bod+"\n}\n";
   if (bod!=&default_body) 
-    delete bod;
+    delete bod; 
 }
 
 void aalang_cpp::set_adapter(std::string* ada,const char* file,int line,int col)
 {
   if (tag) {
     tag_adapter[tag_cnt]=true;
-    s+=to_line(file,line)+"int tag" + to_string(tag_cnt) + "_adapter() {\n" +
-      *ada + "\n"
+    s+=to_line(file,line)+"int tag" + to_string(tag_cnt) + "_adapter(const std::string& name) {\n{\n" +
+      *ada + "\n}\n"
       "\treturn " + to_string(tag_cnt) + ";\n"
       "}\n";    
   } else {
-    s+=to_line(file,line)+"int action" + to_string(action_cnt) + "_adapter(const char* param) {\n" +
-      *ada + "\n"
+    s+=to_line(file,line)+"int action" + to_string(action_cnt) + "_adapter(const char* param,const std::string& name) {\n{\n" +
+      *ada + "\n}\n"
       "\treturn " + to_string(action_cnt) + ";\n"
       "}\n";
   }
@@ -235,11 +246,14 @@ void aalang_cpp::next_action()
   guard_call_construct.pop_back();
   name_cnt=name_cnt_stack.back();
   name_cnt_stack.pop_back();
+  action_cnt=name_cnt_stack.back();
+  name_cnt_stack.pop_back();  
 }
 
 std::string aalang_cpp::stringify()
 {
-
+  action_cnt=anames.size();
+  tag_cnt=tname.size();
   s=s+
     "\npublic:\n"
     "\t~_gen_"+*name+"() {}\n"
@@ -286,19 +300,19 @@ std::string aalang_cpp::stringify()
   s=s+"virtual int observe(std::vector<int>&action, bool block){\n"
     "\taction.clear();\n"
     "\tdo {\n"
-    "\tint r;\n";
+    "\tint r=Alphabet::SILENCE;\n";
 
   int obsa=0;
 
   for(int i=1;i<action_cnt;i++) {
     if (anames[i][0]=='o') {
       obsa++;
-      s=s+"\tr=action"+to_string(amap[i])+"_adapter(NULL);\n"
+      s=s+"\tr=action"+to_string(amap[i])+"_adapter(NULL,action_names["+to_string(i)+"]);\n"
 	"\tif (r) { action.push_back(r); return 1;}\n";
     }
   }
   if (!obsa) {
-    s=s+"\treturn Alphabet::SILENCE;\n";
+    s=s+"\treturn r;\n";
   }
   s=s+"\t} while(block);"
     "\treturn 0;\n"
@@ -311,7 +325,7 @@ std::string aalang_cpp::stringify()
     if (anames[i][0]=='i') {
       s+="\t\tcase "+to_string(i)+":\n"
 	"\t\treturn action"+to_string(amap[i])+
-	"_adapter(param);\n\t\tbreak;\n";
+	"_adapter(param,action_names["+to_string(i)+"]);\n\t\tbreak;\n";
     }
   }
   s=s+"\t\tdefault:\n"
@@ -323,7 +337,7 @@ std::string aalang_cpp::stringify()
 
   for(int i=1;i<action_cnt;i++) {
     s+="\t\tcase "+to_string(i)+":\n"
-      "\t\taction"+to_string(amap[i])+"_body();\n\t\treturn "+
+      "\t\taction"+to_string(amap[i])+"_body(action_names["+to_string(i)+"]);\n\t\treturn "+
       to_string(i)+";\n\t\tbreak;\n";
   }
   s=s+"\t\tdefault:\n"
@@ -350,7 +364,7 @@ std::string aalang_cpp::stringify()
       std::string tnr=to_string(i);
       s=s+"if (std::find(tag.begin(),tag.end(),"+tnr+")!=tag.end()) {\n"
 	"// Tag"+tnr+" adapter\n"+
-	"if (!tag"+tnr+"_adapter()) {\n"+
+	"if (!tag"+tnr+"_adapter(tag_names["+tnr+"])) {\n"+
 	"  t.push_back("+tnr+");\n"+
 	"}\n"+
 	"}\n";
