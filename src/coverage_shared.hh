6790c8ea43532cac2f0c8dc43f33548a6e914f88
Pablo Virolainen <pablo.virolainen@intel.com>
2013-10-04 14:11:29 +0300

    memory leak...

--- src/coverage_shared.hh
+++ src/coverage_shared.hh
@@ -121,6 +121,19 @@ public:
   }
 
   virtual ~Coverage_shared() {
+    if (d_stdin) {
+      g_io_channel_shutdown(d_stdin,TRUE,NULL);
+      g_io_channel_unref(d_stdin);
+    }
+    if (d_stdout) {
+      g_io_channel_shutdown(d_stdout,TRUE,NULL);
+      g_io_channel_unref(d_stdout);
+    }
+    if (d_stderr) {
+      g_io_channel_shutdown(d_stderr,TRUE,NULL);
+      g_io_channel_unref(d_stderr);
+    }
+
     if (child) 
       delete child;
   }
