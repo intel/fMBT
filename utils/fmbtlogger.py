4e1e2419a122241e604a3b52ac1c664077ee3d21
Antti Kervinen <antti.kervinen@intel.com>
2014-03-21 10:18:47 +0200

    fmbtlogger: log formatting improvements and doc
    linePrefix and callPrefix options added to fmbtlogger.text() and
    fmbtlogger.csv(). Timestamp format can be changed with linePrefix.

--- utils/fmbtlogger.py
+++ utils/fmbtlogger.py
@@ -79,12 +79,16 @@ def _formatException(fmt):
     return fmt % values
 
 class FileToLogFunc(object):
-    def __init__(self, fileObj, timeFormat="%s.%f "):
+    def __init__(self, fileObj):
         self._fileObj = fileObj
-        self._timeFormat = timeFormat
+        if hasattr(self._fileObj, "flush"):
+            self._flush = True
+        else:
+            self._flush = False
     def __call__(self, msg):
-        self._fileObj.write("%s%s\n" % (
-                datetime.datetime.now().strftime(self._timeFormat), msg))
+        self._fileObj.write("%s\n" % (msg,))
+        if self._flush:
+            self._fileObj.flush()
 
 class LogWriter(object):
     """
@@ -121,7 +125,8 @@ class LogWriter(object):
 class CSVLogWriter(LogWriter):
     # TODO: fmbtandroid should add here some checks include screenshots
     # to the log where appropriate
-    def __init__(self, logFunc, separator=";", formats=None):
+    def __init__(self, logFunc, separator=";", formats=None,
+                 callPrefix="", linePrefix=""):
         if formats == None:
             self.formats = CSVLogWriter.defaultFormats
         else:
@@ -131,10 +136,20 @@ class CSVLogWriter(LogWriter):
         self.logFunc = logFunc
         self.depth = 0
         self.separator = separator
+        self.callPrefix = callPrefix
+        self.linePrefix = linePrefix
+        if "%" in self.linePrefix:
+            self.timeOnLinePrefix = True
+        else:
+            self.timeOnLinePrefix = False
     def _log(self, msg):
         if len(msg.strip()) > 0:
-            prefix = (self.separator * self.depth)
-            self.logFunc(prefix + msg)
+            columnPrefix = (self.separator * self.depth)
+            if self.timeOnLinePrefix:
+                linePrefix = datetime.datetime.now().strftime(self.linePrefix)
+            else:
+                linePrefix = self.linePrefix
+            self.logFunc(linePrefix + columnPrefix + msg)
     def start(self, actionName):
         msg = _formatAction(self.formats["start"], actionName)
         self._log(msg)
@@ -145,7 +160,7 @@ class CSVLogWriter(LogWriter):
         self.depth -= 1
     def call(self, func, args, kwargs):
         msg = _formatCall(self.formats["call"], func, args, kwargs)
-        self._log(msg)
+        self._log(self.callPrefix + msg)
         self.depth += 1
     def ret(self, returnValue):
         self.depth -= 1
@@ -156,17 +171,95 @@ class CSVLogWriter(LogWriter):
         msg = _formatException(self.formats["exc"])
         self._log(msg)
 
-def csv(obj, logTarget, csvSeparator=";", formats=None, logDepth=1):
+def csv(obj, logTarget, csvSeparator=";", formats=None, logDepth=1,
+        callPrefix="", linePrefix="%s.%f;"):
+    """
+    Get a proxy object for obj. Methods calls, return values and
+    exceptions through the proxy are logged.
+
+    Parameters:
+
+      obj (Python object):
+              Object whose function calls will be logged.
+
+      logTarget (function or file-like object):
+              Function that gets called with one string argument (log
+              message), or a file-like object where log messages will
+              be written.
+
+      csvSeparator (string, optional):
+              separator between CSV columns. The default is ";".
+
+      logDepth (integer, optional):
+              maximum number of nested calls to be logged. The default
+              is 1, that is, only the topmost call (from user code) is
+              logged. -1 is unlimited.
+
+      callPrefix (string, optional):
+              prefix for every logged function call. Handy if several
+              objects are logged to the same logTarget. The default is
+              "".
+
+      linePrefix (string, optional):
+              prefix for every logged line. Prefix can contain
+              strftime conversion specifications. The default is
+              "%s.%f ", that is number of seconds since the Epoch
+              followed by "." and microseconds.
+
+    Returns a proxy object for obj.
+    """
+
     if type(logTarget) == types.FunctionType:
-        logWriter = CSVLogWriter(logTarget, separator=csvSeparator)
+        logWriter = CSVLogWriter(logTarget, separator=csvSeparator,
+                                 callPrefix=callPrefix, linePrefix=linePrefix)
     elif hasattr(logTarget, "write"):
-        logWriter = CSVLogWriter(FileToLogFunc(logTarget), separator=csvSeparator, formats=formats)
+        logWriter = CSVLogWriter(FileToLogFunc(logTarget),
+                                 separator=csvSeparator, formats=formats,
+                                 callPrefix=callPrefix, linePrefix=linePrefix)
     else:
         raise TypeError("logTarget must be a function or a writable file(-like) object")
     return raw(obj, logWriter, logDepth)
 
-def text(obj, logTarget, indentDepth=4, formats=None, logDepth=1):
-    return csv(obj, logTarget, csvSeparator = " "*indentDepth, formats=formats, logDepth=logDepth)
+def text(obj, logTarget, indentDepth=4, formats=None, logDepth=1,
+         callPrefix="", linePrefix="%s.%f "):
+    """
+    Get a proxy object for obj. Methods calls, return values and
+    exceptions through the proxy are logged.
+
+    Parameters:
+
+      obj (Python object):
+              Object whose function calls will be logged.
+
+      logTarget (function or file-like object):
+              Function that gets called with one string argument (log
+              message), or a file-like object where log messages will
+              be written.
+
+      indentDepth (integer, optional):
+              number of spaces when indenting nested log calls.  The
+              default is 4.
+
+      logDepth (integer, optional):
+              maximum number of nested calls to be logged. The default
+              is 1, that is, only the topmost call (from user code) is
+              logged. -1 is unlimited.
+
+      callPrefix (string, optional):
+              prefix for every logged function call. Handy if several
+              objects are logged to the same logTarget. The default is
+              "".
+
+      linePrefix (string, optional):
+              prefix for every logged line. Prefix can contain
+              strftime conversion specifications. The default is
+              "%s.%f ", that is number of seconds since the Epoch
+              followed by "." and microseconds.
+
+    Returns a proxy object for obj.
+    """
+    return csv(obj, logTarget, csvSeparator = " "*indentDepth, formats=formats,
+               logDepth=logDepth, callPrefix=callPrefix, linePrefix=linePrefix)
 
 def raw(obj, logWriter, logDepth=1):
     if not isinstance(logWriter, LogWriter):
