db971d012ab06f2c3c420f5763460e39e4d3f762
Antti Kervinen <antti.kervinen@intel.com>
2015-10-05 13:13:59 +0300

    Visual log: show age only on screenshots

--- utils/fmbtgti.py
+++ utils/fmbtgti.py
@@ -3427,6 +3427,7 @@ else {
         if imgClass: imgClassAttr = 'class="%s" ' % (imgClass,)
         else: imgClassAttr = ""
         imgAge = 0
+        imgAgeAttr = ""
         if isinstance(img, Screenshot):
             #We must use the original screenshot modification time
             try:
@@ -3434,11 +3435,13 @@ else {
             except:
                 #The file returned by unHighlightFilename did not exist.
                 pass
+            if imgAge > 0:
+                imgAgeAttr = ' data-imgage="%s"' % (imgAge,)
             imgHtmlName = self.relFilePath(img.filename(), self._outFileObj)
-            imgHtml = '\n<div class="spacer"><img %s title="%s" src="%s" width="%s" alt="%s" data-imgage="%s"/></div>' % (
+            imgHtml = '\n<div class="spacer"><img %s title="%s" src="%s" width="%s" alt="%s"%s/></div>' % (
                 imgClassAttr,
                 "%s refreshScreenshot() at %s:%s" % img._logCallReturnValue,
-                imgHtmlName, self._screenshotWidth, imgHtmlName, imgAge)
+                imgHtmlName, self._screenshotWidth, imgHtmlName, imgAgeAttr)
         elif img:
             try:
                 imgAge = time.time() - os.stat(self.unHighlightFilename(img)).st_mtime
@@ -3447,11 +3450,13 @@ else {
             if width: width = 'width="%s"' % (width,)
             if type(imgTip) == tuple and len(imgTip) == 3:
                 imgTip = 'title="%s refreshScreenshot() at %s:%s"' % imgTip
+                if imgAge > 0:
+                    imgAgeAttr = ' data-imgage="%s"' % (imgAge,)
             else:
                 imgTip = 'title="%s"' % (imgTip,)
             imgHtmlName = self.relFilePath(img, self._outFileObj)
-            imgHtml = '<div class="spacer"><img %s%s src="%s" %s alt="%s" data-imgage="%s"/></div>' % (
-                imgClassAttr, imgTip, imgHtmlName, width, imgHtmlName, imgAge)
+            imgHtml = '<div class="spacer"><img %s%s src="%s" %s alt="%s"%s/></div>' % (
+                imgClassAttr, imgTip, imgHtmlName, width, imgHtmlName, imgAgeAttr)
         else:
             imgHtml = ""
         return "\n" + imgHtml + "\n"
