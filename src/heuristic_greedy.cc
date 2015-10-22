a5c514865b81dac06e95b8671941fda304c1709d
Pablo Virolainen <pablo.virolainen@intel.com>
2015-10-22 12:36:15 +0300

    heuristic lookahead cleanup and refactor

--- src/heuristic_greedy.cc
+++ src/heuristic_greedy.cc
@@ -75,7 +75,7 @@ Heuristic_greedy::Heuristic_greedy(Log& l,const std::string& params) :
   if (s=="") {
     s="0";
   }
-  
+
   m_search_depth = new_function(s);
 
   if (!m_search_depth) {
@@ -217,48 +217,43 @@ int Heuristic_greedy::getIAction()
     if (!m_burst || m_path.empty() ) {
       /* Use precalculated path (m_path) as a hint. */
       std::reverse(m_path.begin(), m_path.end());
+      std::vector<int> tmp_path = m_path;
 
       double current_score=my_coverage->getCoverage();
       double score;
+      AlgPathToBestCoverage* alg;
 
       /* Spend more time for better coverage */
       if (adaptive) {
-	AlgPathToAdaptiveCoverage alg(m_search_depth->val(), learn, randomise_function);
-	score = alg.search(*model, *my_coverage, m_path);
-	end_condition=(score<=current_score);
-       	if (!alg.status) {
+	alg = new AlgPathToAdaptiveCoverage(m_search_depth->val(), learn, randomise_function);
+      } else {
+	alg = new AlgPathToBestCoverage(m_search_depth->val(), learn, randomise_function);
+      }
+
+      do {
+	m_path = tmp_path;
+	score = alg->search(*model,*my_coverage, m_path,m_search_depth->val());
+
+	if (!alg->status) {
 	  status=false;
-	  errormsg = "Alg: " + alg.errormsg;
+	  errormsg = "Alg: " + alg->errormsg;
 	  retval = 0;
+	  delete alg;
 	  goto done;
 	}
-      } else {
-	AlgPathToBestCoverage alg(m_search_depth->val(), learn, randomise_function);
-	score = alg.search(*model, *my_coverage, m_path);
+	if (score<=current_score) {
+	  log.print("<No improvement at depth %i/>\n",m_search_depth->val());
+	}
+      } while (const_index && score<=current_score && (const_index->stored_val++)<array_size);
+      delete alg;
 
-	do {
-	  score = alg.search(*model,*my_coverage, m_path,m_search_depth->val());
-	  
-	  if (!alg.status) {
-	    status=false;
-	    errormsg = "Alg: " + alg.errormsg;
-
-	    retval = 0;
-	    goto done;
-	  }
-	  if (score<=current_score) {
-	    log.print("<No improvement at depth %i/>\n",m_search_depth->val());
-	  }
-	} while (const_index && score<=current_score && (const_index->stored_val++)<array_size);
-
-	end_condition=(score<=current_score);
-	if (const_index) {
-	  log.print("<depth %i/>\n",m_search_depth->val());
-	  // Next time try a bit smaller value
-	  const_index->stored_val--;
-	  if (const_index->stored_val<0) {
-	    const_index->stored_val=0;
-	  }
+      end_condition=(score<=current_score);
+      if (const_index) {
+	log.print("<depth %i/>\n",m_search_depth->val());
+	// Next time try a bit smaller value
+	const_index->stored_val--;
+	if (const_index->stored_val<0) {
+	  const_index->stored_val=0;
 	}
       }
 
