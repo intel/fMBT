ad0bf9b276166a5447dcadcffc0f24581e88abcf
Antti Kervinen <antti.kervinen@intel.com>
2015-11-11 13:49:32 +0200

    Windows installer: build 32 and 64 installers in Docker

--- docker/build_fmbt_windows_docker.sh
+++ docker/build_fmbt_windows_docker.sh
@@ -1,9 +1,25 @@
-mkdir -p build-win32
+#!/bin/bash
+
+if [ "$1" == "32" ]; then
+    ARCH=32
+elif [ "$1" == "64" ]; then
+    ARCH=64
+else
+    echo "Usage: $0 <32|64>"
+    echo "builds 32 or 64-bit Windows installer"
+    exit 1
+fi
+
+mkdir -p build-win$ARCH
 docker build -t fmbt_fedora .
-docker run -i -v ${PWD}/build-win32:/build-win32 fmbt_fedora sh << COMMANDS
-echo Copying build binaries to host
-cp fMBT/build-win32/*.exe /build-win32
-cp fMBT/build-win32/*.msi /build-win32
+docker run -i -v ${PWD}/build-win$ARCH:/build-win$ARCH fmbt_fedora sh << COMMANDS
+export http_proxy=$http_proxy
+export https_proxy=$https_proxy
+export ftp_proxy=$ftp_proxy
+echo Building $ARCH-bit fMBT installer
+cd fMBT; ./build-fMBT-installer-winXX.exe.sh $ARCH
+echo Copying binaries to host
+cp build-win$ARCH/fMBT-installer-*.exe /build-win$ARCH
 echo Changing owner from \$(id -u):\$(id -g) to $(id -u):$(id -u)
-chown -R $(id -u):$(id -u) /build-win32
+chown -R $(id -u):$(id -u) /build-win$ARCH
 COMMANDS
