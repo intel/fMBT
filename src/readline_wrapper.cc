3081c977b8d831a1d78b563d87257e74b8b6b41f
Antti Kervinen <antti.kervinen@intel.com>
2016-05-27 15:46:30 +0300

    windows: fix interactive mode input

--- src/readline_wrapper.cc
+++ src/readline_wrapper.cc
@@ -2,19 +2,23 @@
 
 #include <cstdio>
 #include <cstdlib>
+#include <cstring>
 
 #if defined(DROI) || defined(__MINGW32__)
 char* READLINE(const char* prompt,FILE* ostream)
 {
     char *s = (char*)malloc(1024);
-    int scanf_status;
-    if (s == NULL) return s;
+    if (s == NULL)
+        return NULL;
     fprintf(ostream, "%s", prompt);
-    scanf_status = scanf("%1023[^\n]", s);
-    scanf("\n");
-    if (scanf_status > 0) return s;
-    std::free(s);
-    return NULL;
+    if (fgets(s, 1024, stdin) == NULL) {
+        // end of input
+        std::free(s);
+        return NULL;
+    }
+    // strip trailing LF, CR, CRLF, LFCR
+    s[strcspn(s, "\r\n")] = 0;
+    return s;
 }
 #else
 #ifdef USE_GNU_READLINE
