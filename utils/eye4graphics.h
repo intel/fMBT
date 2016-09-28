intel/fMBT

commit ce61e7dfc435903dc41437739050561f87ee7ab3

fmbtx11: added support for 16-bit color depth

utils/eye4graphics.h
@@ -249,6 +249,16 @@ extern "C" {
     EXPORT
     int rgbx2rgb(char* data, int width, int height);
 
+
+    /*
+     * rgb5652rgb - convert 16-bit RGB (5-6-5) to 24-bit RGB (8-8-8).
+     *
+     * Return value:
+     *    0: all pixels are black
+     *    > 0: there is at least one non-black pixel
+     */
+    int rgb5652rgb(unsigned short* data_in, int width, int height, char* data_out);
+
     /*
      * bgr2rgb - convert bitmap data BGR to RGB.
      *

utils/eye4graphics.cc
@@ -870,6 +870,25 @@ int bgrx2rgb(char* data, int width, int height)
     return has_nonblack_pixels;
 }
 
+int rgb5652rgb(unsigned short* data_in, int width, int height, char* data_out)
+{
+    const unsigned short red_mask = 0xf800;
+    const unsigned short green_mask = 0x07e0;
+    const unsigned short blue_mask = 0x001f;
+    int has_nonblack_pixels = 0;
+    for (int i = 0; i < height * width; ++i) {
+        unsigned short data16 = data_in[i];
+        unsigned char red = (data16 & red_mask) >> 11;
+        unsigned char green = (data16 & green_mask) >> 5;
+        unsigned char blue = (data16 & blue_mask);
+        if (data16 != 0) has_nonblack_pixels = 1;
+        data_out[3*i] = red << 3;
+        data_out[3*i + 1] = green << 2;
+        data_out[3*i + 2] = blue << 3;
+    }
+    return has_nonblack_pixels;
+}
+
 int rgbx2rgb(char* data, int width, int height)
 {
     int has_nonblack_pixels = 0;

utils/fmbtgti.py
@@ -279,6 +279,12 @@ for _dirname in _libpath:
         eye4graphics.openImage.restype = ctypes.c_void_p
         eye4graphics.openedImageDimensions.argtypes = [ctypes.c_void_p, ctypes.c_void_p]
         eye4graphics.closeImage.argtypes = [ctypes.c_void_p]
+        eye4graphics.rgb5652rgb.restype = ctypes.c_int
+        eye4graphics.rgb5652rgb.argtypes = [
+            ctypes.c_void_p,
+            ctypes.c_int,
+            ctypes.c_int,
+            ctypes.c_void_p]
         break
     except: pass
 else:

utils/fmbtpng.py
@@ -125,12 +125,17 @@ def raw2png(data, width, height, depth=8, fmt="RGB"):
     else:
         buf = data.contents
 
-    buf_addr = ctypes.addressof(buf)
-
     fmt = fmt.upper()
     if fmt == "RGB":
         color_type = PNG_COLOR_TYPE_RGB
         bytes_per_pixel = (depth / 8) * 3
+    elif fmt == "RGB565":
+        color_type = PNG_COLOR_TYPE_RGB
+        bytes_per_pixel = 3
+        depth = 8
+        orig_buf = buf
+        buf = ctypes.create_string_buffer(width * height * bytes_per_pixel)
+        fmbtgti.eye4graphics.rgb5652rgb(orig_buf, width, height, buf)
     elif fmt == "RGBA":
         color_type = PNG_COLOR_TYPE_RGB_ALPHA
         bytes_per_pixel = (depth / 8) * 4
@@ -148,6 +153,8 @@ def raw2png(data, width, height, depth=8, fmt="RGB"):
         bytes_per_pixel = (depth / 8) * 3
     else:
         raise ValueError('Unsupported data format "%s", use "RGB" or "RGBA"')
+
+    buf_addr = ctypes.addressof(buf)
 
     libpng.png_set_IHDR(
         png_struct, info_struct, width, height, depth,

utils/fmbtx11_conn.py
@@ -273,7 +273,10 @@ class Display(object):
             compressed_image = rawfmbt_header + zlib.compress(rawfmbt_data, 3)
         elif fmt.upper() == "PNG" and fmbtpng != None:
             rawdata = ctypes.string_at(image.data, image.height * image.bytes_per_line)
-            compressed_image = fmbtpng.raw2png(rawdata, image.width, image.height, image.bits_per_pixel / 4, "BGR_")
+            if image.bits_per_pixel == 16:
+                compressed_image = fmbtpng.raw2png(rawdata, image.width, image.height, 5, "RGB565")
+            else:
+                compressed_image = fmbtpng.raw2png(rawdata, image.width, image.height, image.bits_per_pixel / 4, "BGR_")
         else:
             compressed_image = None
 

commit 2d45dbe5fe4497b7961b4c94ef6f8699dc4e221a

fmbtgti: fix importing fmbtgti on Windows
Fixes "cannot load eye4graphics.dll" on Windows, broken since ce61e7d.

utils/eye4graphics.h
@@ -257,6 +257,7 @@ extern "C" {
      *    0: all pixels are black
      *    > 0: there is at least one non-black pixel
      */
+    EXPORT
     int rgb5652rgb(unsigned short* data_in, int width, int height, char* data_out);
 
     /*