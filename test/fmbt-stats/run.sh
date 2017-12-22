37515d44994b3f64c2964d2a51048e512a27472c
Antti Kervinen <antti.kervinen@intel.com>
2017-12-22 07:17:23 +0200

    fmbt-stats: update fmbt-stats test to new syntax

--- test/fmbt-stats/run.sh
+++ test/fmbt-stats/run.sh
@@ -35,28 +35,28 @@ fi
 
 cat > test.conf <<EOF
 model = "lsts(model.lsts)"
-inconc = "steps:10000"
+inconc = "steps(10000)"
 EOF
 fmbt test.conf -l stats-input-10000.log >>$LOGFILE 2>&1
 cat > test.conf <<EOF
 model = "lsts(model.lsts)"
-inconc = "steps:100"
+inconc = "steps(100)"
 EOF
 fmbt test.conf -l stats-input-100.log >>$LOGFILE 2>&1
 cat > test.conf <<EOF
 model = "lsts(model.lsts)"
-inconc = "steps:2"
+inconc = "steps(2)"
 EOF
 fmbt test.conf -l stats-input-2.log >>$LOGFILE 2>&1
 cat > test.conf <<EOF
 model = "lsts(model.lsts)"
-inconc = "steps:1"
+inconc = "steps(1)"
 EOF
 fmbt test.conf -l stats-input-1.log >>$LOGFILE 2>&1
 cat > test.conf <<EOF
 model = "lsts(model.lsts)"
 adapter = "remote_lsts:echo 'this wont work'"
-inconc = "steps:0"
+inconc = "steps(0)"
 EOF
 fmbt test.conf -l stats-input-0.log >>$LOGFILE 2>&1
 testpassed
@@ -69,9 +69,9 @@ adapter   = "remote(remote_python -l adapter.log
                  -c 'from teststeps import *')"
 heuristic = "lookahead(4)"
 coverage  = "perm(1)"
-inconc    = "coverage:1.1"
-pass      = "no_progress:6"
-on_fail   = "exit:1"
+inconc    = "coverage(1.1)"
+pass      = "no_progress(6)"
+on_fail   = "exit(1)"
 EOF
 fmbt quick.conf -l quick.log >>$LOGFILE 2>&1 || {
     testfailed
