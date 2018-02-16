c365a4f0014d297bea9f163ef1c5804334bcd400
Antti Kervinen <antti.kervinen@intel.com>
2018-02-16 11:03:15 +0200

    pycosh: dir sep / in prompt on all platforms

--- utils/pycosh.py
+++ utils/pycosh.py
@@ -139,7 +139,7 @@ def prompt():
         hostname = ""
     return (user + "@" +
             hostname + ":" +
-            os.getcwd() + ": ")
+            os.getcwd().replace("\\", "/") + ": ")
 
 def awk(prog, *args):
     """awk PROG [FILE...]
