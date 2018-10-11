6d6312b9baece87b4fd9185aca5703174388631e
Antti Kervinen <antti.kervinen@intel.com>
2018-10-11 10:54:43 +0300

    Version 0.40

--- pythonshare/setup.py
+++ pythonshare/setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name         = 'pythonshare',
-      version      = '0.39',
+      version      = '0.40',
       description  = 'Persistent, shared and distributed Python namespaces',
       author       = 'Antti Kervinen',
       author_email = 'antti.kervinen@intel.com',
