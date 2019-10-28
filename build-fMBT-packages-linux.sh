daa8e4a0d39bf6e5f8fdeb7c59b9aeafe97566db
jukka2 <jukka@dashing.fi>
2019-10-28 21:25:03 +0200

    build python3share debs in ci
    modify python3 and python-dh version requirement
    to be able to build in debian stretch

--- build-fMBT-packages-linux.sh
+++ build-fMBT-packages-linux.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-apt-get update; apt-get -y install git dpkg-dev devscripts python debhelper gcc libglib2.0-dev libboost-dev libedit-dev automake libtool libxml2-dev libmagickcore-dev libboost-regex-dev libc6-dev python-dev flex dbus python-pexpect python-dbus python-gobject graphviz imagemagick
+apt-get update; apt-get -y install git dpkg-dev devscripts python debhelper gcc libglib2.0-dev libboost-dev libedit-dev automake libtool libxml2-dev libmagickcore-dev libboost-regex-dev libc6-dev python-dev flex dbus python-pexpect python-dbus python-gobject graphviz imagemagick python3 dh-python
 echo "detected fMBT version $(git describe --tags | sed 's/v//1')"
 dch --empty -b -v $(git describe --tags | sed 's/v//1') --distribution bionic --force-distribution Build by https://gitlab.com/fmbt/fmbt_ci/
 
@@ -16,3 +16,18 @@ mkdir debs
 mv ../*.deb debs
 mv ../*.changes debs
 mv ../*.dsc debs
+
+cd python3share
+dch --empty -b -v $(git describe --tags | sed 's/v//1') --distribution bionic --force-distribution Build by https://gitlab.com/fmbt/fmbt_ci/
+dpkg-buildpackage -us -uc
+EXIT_CODE=$?
+ls -la
+if [ $EXIT_CODE != 0 ]; then
+    mv /tmp/fmbt*.log logs
+    exit ${EXIT_CODE}
+fi
+
+mv ../*.deb ../debs
+mv ../*.changes ../debs
+mv ../*.dsc ../debs
+
